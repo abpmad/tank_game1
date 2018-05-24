eeae0ee50589581624ae8a22c8e053e3c169ffa4
abpmad <donghe1216@gmail.com>
2018-05-24 00:13:02 -0700

    version 0.2

--- src/com/mad/tankgame/TankClient.java
+++ src/com/mad/tankgame/TankClient.java
@@ -1,11 +1,20 @@
 package com.mad.tankgame;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class TankClient extends Frame{
 	
 	public void launchFrame(){
 		this.setLocation(400, 300);
 		this.setSize(800 , 600);
+		this.setTitle("TankWar");
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		this.setResizable(false);
 		setVisible(true);
 	}
 	public static void main(String[] args) {
