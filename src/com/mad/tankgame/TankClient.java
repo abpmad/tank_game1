1cda3bd53d8d860dba8cf6011e339ae7e03f4128
abpmad <donghe1216@gmail.com>
2019-10-15 08:20:56 -0700

    test

--- src/com/mad/tankgame/TankClient.java
+++ src/com/mad/tankgame/TankClient.java
@@ -4,6 +4,17 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankClient extends Frame{
+	int x = 50, y = 50;
+	
+	public void paint(Graphics g) {
+		Color c = g.getColor();
+		g.setColor(Color.RED);
+		g.fillOval(x, y, 30, 30);
+		g.setColor(c);
+		
+		y += 5;
+		x += 5;
+	}
 	
 	public void launchFrame(){
 		this.setLocation(400, 300);
@@ -15,11 +26,33 @@ public class TankClient extends Frame{
 			}
 		});
 		this.setResizable(false);
+		this.setBackground(Color.GREEN);
 		setVisible(true);
+		new Thread(new TankThread()).start();
 	}
+	
 	public static void main(String[] args) {
 		TankClient tc = new TankClient();
 		tc.launchFrame();
+		
+	}
+	
+	private class TankThread implements Runnable{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			while(true){
+				repaint();
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		
+		}
 		
 	}
 
