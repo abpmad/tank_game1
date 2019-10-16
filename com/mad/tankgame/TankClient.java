1569ec5a27b4c99be8ab29ea7efc1d5359f02a65
abpmad <donghe1216@gmail.com>
2019-10-15 22:48:11 -0700

    version 0.4

--- com/mad/tankgame/TankClient.java
+++ com/mad/tankgame/TankClient.java
@@ -5,6 +5,9 @@ import java.awt.event.WindowEvent;
 
 public class TankClient extends Frame{
 	int x = 50, y = 50;
+	public static final int GAME_WIDTH = 800;
+	public static final int GAME_HEIGHT = 600;
+	Image offScreenImage = null;
 	
 	public void paint(Graphics g) {
 		Color c = g.getColor();
@@ -16,9 +19,25 @@ public class TankClient extends Frame{
 		x += 5;
 	}
 	
+	@Override
+	public void update(Graphics g) {
+		if( offScreenImage == null ){
+			offScreenImage = createImage(GAME_WIDTH, GAME_HEIGHT);
+		}
+		
+		Graphics gOffScreen = offScreenImage.getGraphics();
+		Color c = gOffScreen.getColor();
+		gOffScreen.setColor(Color.GREEN);
+		gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+		gOffScreen.setColor(c);
+		paint(gOffScreen);
+		g.drawImage(offScreenImage, 0, 0, null);
+		
+	}
+
 	public void launchFrame(){
 		this.setLocation(400, 300);
-		this.setSize(800 , 600);
+		this.setSize(GAME_WIDTH , GAME_HEIGHT);
 		this.setTitle("TankWar");
 		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
